a023fe9d9f0e35bf221be9b5ad75f337ca1a7a63
Daniel Maitre <daniel.maitre@durham.ac.uk>
2016-04-12 09:31:42 -0700

    better argument treatment

--- nTuple2Rivet.cpp
+++ nTuple2Rivet.cpp
@@ -6,8 +6,62 @@
 
 int main(int argc,char *argv[]){
 
+
+  int index;
+  int c;
+  std::string analysis("unset");
+  std::string outfile("unset");
+  opterr = 0;
+
+  int nPoints=10000000000;
+
+  
+  while ((c = getopt (argc, argv, "a:o:n:")) != -1)
+    switch (c)
+      {
+      case 'a':
+	analysis=std::string(optarg);
+        break;
+      case 'o':
+	outfile=std::string(optarg);
+        break;
+      case 'n':
+        nPoints = std::stoi(optarg);
+        break;
+      case '?':
+        if (optopt == 'n' || optopt == 'a' || optopt == 'o' )
+          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+        else if (isprint (optopt))
+          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+        else
+          fprintf (stderr,
+                   "Unknown option character `\\x%x'.\n",
+                   optopt);
+        return 1;
+      default:
+        abort ();
+      }
+
+
+  if (analysis=="unset"){
+    printf("Need an analysis! Please set it with the -a option");
+    abort();
+  }
+  if (outfile=="unset"){
+    outfile=analysis+".yoda";
+  }
+
+
+  printf ("will run with parameters :\n  analysis = %s, output file = %s, nPoints = %d\n",
+          analysis.c_str(), outfile.c_str() , nPoints);
+
+
+
+
+
+  
   Rivet::AnalysisHandler rivet;
-  rivet.addAnalysis(argv[1]);
+  rivet.addAnalysis(analysis.c_str());
   if (rivet.analysisNames().size() != 1) {
     std::cerr << "No analysis " << argv[1] << " found! Try export RIVET_ANALYSIS_PATH=THEPATHWHEREYOUHAVETHEANALYSIS" << std::endl;
     return 1;
@@ -15,8 +69,9 @@ int main(int argc,char *argv[]){
 
   nTupleReader r;
 
-  for (int ii=2;ii<argc;ii++){
-    r.addFile(argv[ii]);
+  for (index = optind; index < argc; index++){
+    printf ("nTuple file %s added!\n", argv[index]);
+    r.addFile(argv[index]);
   }
 
   //  r.setPDF("CT10nlo.LHgrid");
@@ -27,7 +82,7 @@ int main(int argc,char *argv[]){
 
   long nE=0;
 
-  while(r.nextEntry()){
+  while(r.nextEntry() && nE<nPoints){
     HepMC::GenEvent evt;
     r.fillHepMC(evt,r.getCrossSection(),r.getCrossSectionError());
     rivet.analyze(evt);
@@ -41,7 +96,7 @@ int main(int argc,char *argv[]){
 
   rivet.setCrossSection(r.getCrossSection());
   rivet.finalize();
-  rivet.writeData(std::string(argv[1])+".yoda");
+  rivet.writeData(outfile);
 
   return 0;
  
